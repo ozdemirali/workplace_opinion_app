73f5b222ecf1aae1d7d5898dcaba73aaf18322be
Ali <ozdemirali@hotmail.com>
2021-03-14 21:52:59 +0300

    List Widget is recreated.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:workplace_opinion_app/widgest/list.dart';
+import 'package:workplace_opinion_app/widgets/list.dart';
 
 class Home extends StatefulWidget{
   @override
